798d97de3718b038da4270bc2a7e9873cf904c1d
Jairo Caro-Accino Viciana <jairo@galax.be>
2017-12-28 18:02:43 +0100

    Parallelizing is better :D
    Parallelizing is better :D

--- crack.go
+++ crack.go
@@ -2,35 +2,41 @@
 package main
 
 // import all the libraries I'll need
-import ( 					
-	"fmt"
-    "os"
+import (
 	"crypto/md5"
 	"encoding/hex"
+	"fmt"
+	"os"
 	"strconv"
 )
 
 // define a string variable "cipher" from the first command line arg
-var cipher string = os.Args[1] 		
+var cipher string = os.Args[1]
 
 func main() {
 	// give a heads up that we're starting
 	fmt.Println("[+] Cracking...")
-    // C-style for loop
+
+	c := make(chan string)
+	// C-style for loop
 	for i := 1; i < 99999999; i++ {
-    	// use strconv to convert integer to string, 
-        // hash the string, then compare it to our provided hash
-		if GetMD5Hash(strconv.Itoa(i)) == cipher { 
-        	// if we get a match, print it out and exit with a zero exit code
-			fmt.Printf("[!] The code has been cracked: %d\n", i)
-			os.Exit(0)
-		}
+		// use strconv to convert integer to string,
+		// hash the string, then compare it to our provided hash
+		go GetMD5Hash(i, cipher, c)
 	}
+
+	i := <-c
+	fmt.Println("[!] The code has been cracked: ", i)
+	os.Exit(0)
 }
 
 // function that takes a string and returns the md5 of that string
-func GetMD5Hash(text string) string { 			
-    hasher := md5.New()
-    hasher.Write([]byte(text))
-    return hex.EncodeToString(hasher.Sum(nil))
+func GetMD5Hash(i int, cipher string, c chan string) {
+	text := strconv.Itoa(i)
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	res := hex.EncodeToString(hasher.Sum(nil))
+	if res == cipher {
+		c <- res
+	}
 }
